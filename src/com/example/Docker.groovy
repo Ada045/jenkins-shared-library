e3d1963222cc0c91cc75ea64405e5f05d5e7a0c7
Adaeze Igbokwe <igbokweadaeze04@gmail.com>
2026-07-22 16:41:04 +0100

    added deploy stage

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -32,4 +32,18 @@ class Docker implements Serializable {
         script.echo 'Pushing image into Docker repo...'
         script.sh "docker push '${imageName}'"
     }
+
+    def deploy(String imageName) {
+
+        def dockerCmd = """
+            docker pull ${imageName}
+            docker stop java-app || true
+            docker rm java-app || true
+            docker run -d --name java-app -p 8081:8081 ${imageName}
+        """
+
+        sshagent(['ec2-server-key']) {
+            script.sh "ssh -o StrictHostKeyChecking=no ec2-user@63.177.99.170 '${dockerCmd}'"
+        }
+    }
 }
